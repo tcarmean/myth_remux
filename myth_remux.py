08cfef73090b7e5bd382c18c24e9f01e898c6dcf
Tim Carmean <chong@technodrome.gondor>
2013-05-20 15:41:32 -0400

    stubbing out method to do the remuxing.

--- myth_remux.py
+++ myth_remux.py
@@ -11,12 +11,31 @@ import uuid
 # Usage should be: myth_remux.py <dir> <file>
 
 # We need exactly 2 args so fail if we don't have them.
-if (len(sys.argv) != 3):
-	exit(1)
 
-orig_dir = sys.argv[1]
-orig_file = sys.argv[2]
-filename = sys.argv[1] + sys.argv[2]
-temp_file = '/tmp/' + base64.urlsafe_b64encode(uuid.uuid4().bytes) + '.mpg'
-print('Remuxing ' + filename + ' as ' + temp_file)
+
+# The first thing we need to do is remux the input file
+def remux( filename, temp_file ):
+	# Here's what we need to call
+	# ffmpeg -i filename -acodec copy -vcodec copy temp_file
+	# We will use subprocess.check_call as described here:
+	# http://docs.python.org/2/library/subprocess.html
+	try:
+		# check_call()
+	except CalledProcessError:
+		print(CalledProcessError.returncode)
+		exit(1)
+
+
+
+
+# Main method. This is the entry point of the application
+if __name__ == "__main__":
+	if (len(sys.argv) != 3):
+		exit(1)
+
+	orig_dir = sys.argv[1]
+	orig_file = sys.argv[2]
+	filename = sys.argv[1] + sys.argv[2]
+	temp_file = '/tmp/' + base64.urlsafe_b64encode(uuid.uuid4().bytes) + '.mpg'
+	print('Remuxing ' + filename + ' as ' + temp_file)
 
